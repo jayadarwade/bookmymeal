b03986dd256bd3b252adcd41c20e8b0de4953a24
Your Name <you@example.com>
2022-03-21 15:44:34 +0530

    change2

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ const path = require("path");
 const adminRouter = require("./routes/admin.route");
 const userRouter = require("./routes/user.route");
 const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://root:root@cluster0.8em2i.mongodb.net/BookMyMeal?retryWrites=true&w=majority"
+);
+
 const port = process.env.PORT || 5555
 
 const bodyParser = require("body-parser");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-mongoose.connect(
-  "mongodb+srv://root:root@cluster0.8em2i.mongodb.net/BookMyMeal?retryWrites=true&w=majority"
-);
 
 module.exports = mongoose.connection;
 
