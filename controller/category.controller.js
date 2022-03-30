5380ed05fd9735b4b1f854a746480cb7a1a3ee84
Your Name <you@example.com>
2022-03-30 17:36:34 +0530

    image

--- controller/category.controller.js
+++ controller/category.controller.js
@@ -8,7 +8,7 @@ exports.addCategory = (request, response, next) => {
   }
   Category.create({
     categoryname: request.body.categoryname,
-    categoryimage: "http://localhost:3000/images/" + request.file.filename,
+    categoryimage: "http://bookmymealjaya.herokuapp.com/images/" + request.file.filename,
   })
     .then((result) => {
       return response.status(201).json(result);
